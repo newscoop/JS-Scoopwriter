c5ed1a176e9163b74f1062fc3e3c1f96f0edcaae
danse <f.occhipinti@gmail.com>
2013-12-06 15:39:17 +0100

    get the article number from the location
    works with a locator like http://127.0.0.1:9000/#/?f_article_number=64
    due to angular hash routing

--- app/scripts/controllers/article.js
+++ app/scripts/controllers/article.js
@@ -1,8 +1,10 @@
 'use strict';
 
 angular.module('authoringEnvironmentApp')
-  .controller('ArticleCtrl', function ($scope, $http) {
-    $http.get('http://tw-merge.lab.sourcefabric.org/api/articles/64')
+  .controller('ArticleCtrl', function ($scope, $http, $location) {
+    var s = $location.search(),
+      n = s.f_article_number;
+    $http.get('http://tw-merge.lab.sourcefabric.org/api/articles/' + n)
       .success(function(data) {
           $scope.article = data;
       });
