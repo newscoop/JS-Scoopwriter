9325c5496e599a7fe2511c84ea2f51e8168b678e
Yorick Terweijden <yorick.terweijden@sourcefabric.org>
2013-12-09 17:10:26 +0100

    Changes

--- app/scripts/services/panes.js
+++ app/scripts/services/panes.js
@@ -6,9 +6,7 @@ angular.module('authoringEnvironmentApp')
     // ...
 
     var panes = [
-        {name: "Topics", icon:'chat', template: 'views/pane-topics.html', position: 'left', active: false, selected: true},
-        {name: "Koekjes", icon:'ingest', template: 'views/pane-topics.html', position: 'left', active: false, selected: true},
-        {name: "Media", icon:'ingest', template: 'views/pane-topics.html', position: 'right', active: false, selected: true}
+        {name: "Topics", icon:'chat', template: 'views/pane-topics.html', position: 'left', active: false, selected: true}
     ];
 
     // Public API here
