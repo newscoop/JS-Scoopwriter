07200eeff8eda688d41ca029bcf131bce0a2ca94
danse <f.occhipinti@gmail.com>
2013-12-09 14:17:13 +0100

    add flexibility about article fields

--- app/scripts/controllers/article.js
+++ app/scripts/controllers/article.js
@@ -2,10 +2,34 @@
 
 angular.module('authoringEnvironmentApp')
   .controller('ArticleCtrl', function ($scope, $http, $location) {
-    var s = $location.search(),
+    var
+      //b = 'http://tw-merge.lab.sourcefabric.org', // standalone
+      b = '', // plugin
+      s = $location.search(),
       n = s.f_article_number;
-    $http.get('/api/articles/' + n)
-      .success(function(data) {
-          $scope.article = data;
+    $http
+          .get(b + '/api/articles/' + n)
+          .success(function(article) {
+              $http
+                  .get(b + '/api/articleTypes/' + article.type)
+                  .success(function(type) {
+                      $scope.type = type;
+                      $scope.article = article;
+                  });
       });
+      // used to filter
+      $scope.editable = function(field) {
+          if (field.isContentField == 0) {
+              return false;
+          }
+          var known = [
+              'text',
+              'long_text',
+              'body'
+          ];
+          if (known.indexOf(field.type) == -1) {
+              return false;
+          };
+          return true;
+      };
   });
