2f8f7d518c44600c8c91c699c21fd2e331f1c9aa
danse <f.occhipinti@gmail.com>
2013-12-10 15:30:44 +0100

    load dependencies for the tests

--- karma.conf.js
+++ karma.conf.js
@@ -12,6 +12,7 @@ module.exports = function(config) {
     // list of files / patterns to load in the browser
     files: [
       'app/bower_components/angular/angular.js',
+      'app/bower_components/jquery/jquery.js',
       'app/bower_components/angular-mocks/angular-mocks.js',
       'app/bower_components/angular-resource/angular-resource.js',
       'app/bower_components/angular-cookies/angular-cookies.js',
@@ -22,7 +23,11 @@ module.exports = function(config) {
       'app/scripts/controllers/*.js',
       'app/scripts/directives/*.js',
       'test/mock/**/*.js',
-      'test/spec/**/*.js'
+      'test/spec/**/*.js',
+      'app/scripts/aloha/lib/require.js',
+      'app/scripts/aloha/lib/vendor/jquery-1.7.2.js',
+      'app/scripts/aloha/lib/aloha.js',
+      'app/bower_components/ng-aloha-editor/ng-aloha-editor.js',
     ],
 
     // list of files / patterns to exclude
