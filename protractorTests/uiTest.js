c56a63586d74e263b2e72ead201f5fda19a9a2e1
Yorick Terweijden <yorick.terweijden@sourcefabric.org>
2014-01-16 14:09:01 +0100

    Checks if the pane itself has the active class set

--- protractorTests/uiTest.js
+++ protractorTests/uiTest.js
@@ -5,36 +5,42 @@ describe('the editor starting page', function() {
 
     it('should resize the article view if a pane is opened on the left', function() {
         var mainArticle = element(by.id('main-article'));
-        var tablistleft = element(by.className('left-tablist'));
-        var anchorElementLeft = tablistleft.findElement(by.tagName('a'));
+        var tablistLeft = element(by.className('left-tablist'));
+        var paneLeft = tablistLeft.findElement(by.tagName('li'));
+        var anchorElementLeft = tablistLeft.findElement(by.tagName('a'));
 
         // open the pane
         anchorElementLeft.click();
 
+        expect(paneLeft.getAttribute('class')).toMatch('active');
         expect(mainArticle.getAttribute('class')).toMatch('shrink-left');
         expect(mainArticle.getAttribute('class')).not.toMatch('shrink-right');
 
         // close the pane
         anchorElementLeft.click();
 
+        expect(paneLeft.getAttribute('class')).not.toMatch('active');
         expect(mainArticle.getAttribute('class')).not.toMatch('shrink-left');
         expect(mainArticle.getAttribute('class')).not.toMatch('shrink-right');
     });
 
     it('should resize the article view if a pane is opened on the right', function() {
         var mainArticle = element(by.id('main-article'));
-        var tablistright = element(by.className('right-tablist'));
-        var anchorElementRight = tablistright.findElement(by.tagName('a'));
+        var tablistRight = element(by.className('right-tablist'));
+        var paneRight = tablistRight.findElement(by.tagName('li'));
+        var anchorElementRight = tablistRight.findElement(by.tagName('a'));
 
         // open the pane
         anchorElementRight.click();
 
+        expect(paneRight.getAttribute('class')).toMatch('active');
         expect(mainArticle.getAttribute('class')).toMatch('shrink-right');
         expect(mainArticle.getAttribute('class')).not.toMatch('shrink-left');
 
         // close the pane
         anchorElementRight.click();
 
+        expect(paneRight.getAttribute('class')).not.toMatch('active');
         expect(mainArticle.getAttribute('class')).not.toMatch('shrink-right');
         expect(mainArticle.getAttribute('class')).not.toMatch('shrink-left');
     });
