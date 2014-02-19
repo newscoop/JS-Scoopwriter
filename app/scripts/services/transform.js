06f700de12dc96e9ef42f07aa09dde7e2499203b
Francesco Occhipinti <francesco.occhipinti@sourcefabric.org>
2014-02-19 17:54:15 +0100

    better variable names

--- app/scripts/services/transform.js
+++ app/scripts/services/transform.js
@@ -3,10 +3,10 @@
 angular.module('authoringEnvironmentApp')
     .service('transform', function Transform() {
         // AngularJS will instantiate a singleton by calling "new" on this function
-        this.formEncode = function(data, headers) {
-            var d = $.param(data);
-            var h = headers();
-            h['Content-Type'] = 'application/x-www-form-urlencoded';
-            return d;
+        this.formEncode = function(data, headersGetter) {
+            var encoded = $.param(data);
+            var headers = headersGetter();
+            headers['Content-Type'] = 'application/x-www-form-urlencoded';
+            return encoded;
         };
     });
