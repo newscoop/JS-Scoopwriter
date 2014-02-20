c1e408ba10649449e8ebd2b4c2f83a25f8e805cb
Francesco Occhipinti <francesco.occhipinti@sourcefabric.org>
2014-02-20 17:29:34 +0100

    AENV-100 add comment filters by status

--- app/scripts/controllers/comments.js
+++ app/scripts/controllers/comments.js
@@ -2,6 +2,50 @@
 
 angular.module('authoringEnvironmentApp')
     .controller('CommentsCtrl', ['$scope', 'comments', function ($scope, comments) {
+        var others = ['new', 'approved', 'hidden'];
+        $scope.toggle = function(name) {
+            var previouslyChecked = $scope.statuses[name];
+            $scope.statuses[name] = !previouslyChecked;
+            if ('all' == name) {
+                if (previouslyChecked) {
+                    others.map(function(name) {
+                        $scope.statuses[name] = true;
+                    });
+                } else {
+                    others.map(function(name) {
+                        $scope.statuses[name] = false;
+                    });
+                }
+            } else {
+                /* if this is going to be unchecked (so it was checked
+                 * before) and all the others are unchecked, check the
+                 * `all` status again */
+                if (previouslyChecked) {
+                    if (others.every(function(name) {
+                        return false == $scope.statuses[name];
+                    })) {
+                        $scope.statuses.all = true;
+                    }
+                } else {
+                    $scope.statuses.all = false;
+                }
+            }
+        };
+        $scope.statuses = {
+            all: true,
+            'new': false, // reserved word
+            approved: false,
+            hidden: false
+        };
+        $scope.filter = function(comment) {
+            if ($scope.statuses.all) {
+                return true;
+            }
+            if ($scope.statuses[comment.status]) {
+                return true;
+            }
+            return false;
+        };
         $scope.comments = comments;
         $scope.create = {};
         comments.init();
--- test/spec/controllers/comments.js
+++ test/spec/controllers/comments.js
@@ -9,6 +9,17 @@ describe('Controller: CommentsCtrl', function () {
     scope,
     commentsService = {
         init: function() {}
+    },
+    comments = {
+        'new': {
+            status: 'new',
+        },
+        approved: {
+            status: 'approved',
+        },
+        any: {
+            status: 'whatever'
+        }
     };
 
     // Initialize the controller and a mock scope
@@ -22,5 +33,76 @@ describe('Controller: CommentsCtrl', function () {
 
     it('proxies comments', function () {
         expect(scope.comments).toBeDefined();
+    });
+    it('has the new filter unchecked', function() {
+        expect(scope.statuses.new).toBe(false);
+    });
+    it('does not filter', function() {
+        expect(scope.filter(comments.any)).toBe(true);
+    });
+    describe('the `all` status', function() {
+        var status;
+        beforeEach(function() {
+            status = scope.statuses.all;
+        });
+        it('is checked', function() {
+            expect(status).toBe(true);
+        });
+    });
+    describe('new comments selected', function() {
+        beforeEach(function() {
+            scope.toggle('new');
+        });
+        it('checks the `new` filter', function() {
+            expect(scope.statuses['new']).toBe(true);
+        });
+        it('unchecks the `all` filter', function() {
+            expect(scope.statuses.all).toBe(false);
+        });
+        it('accepts `new` comments', function() {
+            expect(scope.filter(comments['new'])).toBe(true);
+        });
+        it('rejects other comments', function() {
+            expect(scope.filter(comments.any)).toBe(false);
+        });
+        describe('new comments unselected', function() {
+            beforeEach(function() {
+                scope.toggle('new');
+            });
+            it('activates all again', function() {
+                expect(scope.statuses.all).toBe(true);
+            });
+        });
+        describe('approved comments selected', function() {
+            beforeEach(function() {
+                scope.toggle('approved');
+            });
+            it('accepts `new` comments', function() {
+                expect(scope.filter(comments.new)).toBe(true);
+            });
+            it('accepts `approved` comments', function() {
+                expect(scope.filter(comments.approved)).toBe(true);
+            });
+            it('rejects other comments', function() {
+                expect(scope.filter(comments.any)).toBe(false);
+            });
+            describe('all comments selected', function() {
+                beforeEach(function() {
+                    scope.toggle('all');
+                });
+                it('accepts all comments', function() {
+                    expect(scope.filter(comments.any)).toBe(true);
+                });
+                it('checks `all`', function() {
+                    expect(scope.statuses.all).toBe(true);
+                });
+                it('unchecks `new`', function() {
+                    expect(scope.statuses['new']).toBe(false);
+                });
+                it('unchecks `approved`', function() {
+                    expect(scope.statuses.approved).toBe(false);
+                });
+            });
+        });
     });
 });
