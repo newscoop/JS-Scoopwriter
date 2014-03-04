035d5027af58c3ad5379875f0dd77eab62b58764
Peter Lamut <peter.lamut@niteoweb.com>
2014-03-04 14:01:14 +0100

    Add angular-animate as a dependency

--- karma.conf.js
+++ karma.conf.js
@@ -22,6 +22,7 @@ module.exports = function(config) {
       'app/bower_components/angular-cookies/angular-cookies.js',
       'app/bower_components/angular-sanitize/angular-sanitize.js',
       'app/bower_components/angular-route/angular-route.js',
+      'app/bower_components/angular-animate/angular-animate.js',
       'app/bower_components/angular-strap/dist/modules/dimensions.min.js',
       'app/bower_components/angular-strap/dist/modules/tooltip.min.js',
       'app/bower_components/angular-strap/dist/modules/popover.min.js',
