62a9985b2205c86dc1b0166eb7f2adbaacc15dc1
Peter Lamut <peter.lamut@niteoweb.com>
2014-03-11 12:25:23 +0100

    Add YUIDoc comments to the 'transform' service.

--- app/scripts/services/transform.js
+++ app/scripts/services/transform.js
@@ -1,8 +1,28 @@
 'use strict';
 
+/**
+* AngularJS Service for encoding HTML form data, making the latter suitable
+* for use in a URL query string or Ajax request.
+*
+* @class transform
+*/
 angular.module('authoringEnvironmentApp')
     .service('transform', function Transform() {
         // AngularJS will instantiate a singleton by calling "new" on this function
+
+        /**
+        * Encodes HTML form data (making it ready to use in an Ajax
+        * request) and sets the Content-Type header to
+        * 'application/x-www-form-urlencoded'.
+        *
+        * @method formEncode
+        * @param data {Object} An object containing form data.
+        * @param headersGetter {Function} A function which returns a list of
+        *   HTTP headers (in a form of an object whose keys are header names
+        *   and the values are the values of the corresponding HTTP headers).
+        *
+        * @return {String} Encoded form data (using jQuery's param() function).
+        */
         this.formEncode = function(data, headersGetter) {
             var encoded = $.param(data);
             var headers = headersGetter();
