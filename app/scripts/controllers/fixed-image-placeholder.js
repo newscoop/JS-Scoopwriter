be55822140a1ee8103eff3a5f88d16bdd9b92bb7
Francesco Occhipinti <francesco.occhipinti@sourcefabric.org>
2014-03-14 12:37:54 +0100

    AENV-134 remove broken link

--- app/scripts/controllers/fixed-image-placeholder.js
+++ app/scripts/controllers/fixed-image-placeholder.js
@@ -3,15 +3,15 @@
 angular.module('authoringEnvironmentApp')
     .controller('FixedImagePlaceholderCtrl', ['$scope', 'images', 'configuration', function ($scope, images, configuration) {
 	$scope.root = configuration.API.rootURI;
-        $scope.style = {
-            opacity: 0
-        };
+        $scope.style = {}; // currently unused
+        $scope.dropped = false;
         $scope.onDrop = function(data) {
             var parsed = JSON.parse(data);
             if ('image' == parsed.type) {
                 $scope.$apply(function() {
                     $scope.image = images.byId(parsed.id);
                     $scope.style.opacity = 1;
+                    $scope.dropped = true;
                 });
             }
         };
