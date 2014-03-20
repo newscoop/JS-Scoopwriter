87c59fc80fb8a4bfa537de6a9d6a17ab4397e14d
Peter Lamut <peter.lamut@niteoweb.com>
2014-03-20 11:49:15 +0100

    Fix sorting comments chronologically

--- test/spec/filters/sort-comments.js
+++ test/spec/filters/sort-comments.js
@@ -1,38 +1,80 @@
 'use strict';
 
+/**
+* Module with tests for the sortComment filter.
+*
+* @module sortComments filter tests
+*/
+
 describe('Filter: sortComments', function () {
 
     var mock = [{
         created:"2013-04-17T20:25:53+0000",
-        thread_order: 3,
+        thread_order: 3
     }, {
         created:"2013-04-17T23:53:19+0000",
-        thread_order: 1,
+        thread_order: 1
     }, {
         created:"2013-04-18T04:04:55+0000",
-        thread_order: 2,
+        thread_order: 2
+    }, {
+        created:"2013-02-12T16:22:45+0000",
+        thread_order: 5
+    }, {
+        created:"2013-04-19T04:04:55+0000",
+        thread_order: 4
     }];
 
     // load the filter's module
     beforeEach(module('authoringEnvironmentApp'));
 
-    // initialize a new instance of the filter before each test
-    var sortComments;
-    beforeEach(inject(function ($filter) {
-        sortComments = $filter('sortComments');
-    }));
+    describe('"Nested" version', function () {
+        var filterFactory;
 
-    it('does thread sorting', function () {
-        var sorted = sortComments(mock, 'Nested');
-        expect(sorted[0].thread_order).toBe(1);
-        expect(sorted[1].thread_order).toBe(2);
-        expect(sorted[2].thread_order).toBe(3);
+        beforeEach(inject(function ($filter) {
+            filterFactory = $filter('sortComments');
+        }));
+
+        it('sorts comments by thread order', function () {
+            var i,
+                sorted = filterFactory(mock, 'Nested');
+
+            for (i = 0; i < sorted.length; i++) {
+                expect(sorted[i].thread_order).toBe(i + 1);
+            }
+        });
     });
-    it('does chronological sorting', function () {
-        var sorted = sortComments(mock, 'Chronological');
-        expect(sorted[0].created).toBe("2013-04-18T04:04:55+0000");
-        expect(sorted[1].created).toBe("2013-04-17T23:53:19+0000");
-        expect(sorted[2].created).toBe("2013-04-17T20:25:53+0000");
+
+    describe('"Chronological" version', function () {
+        var filterFactory;
+
+        beforeEach(inject(function ($filter) {
+            filterFactory = $filter('sortComments');
+        }));
+
+        it('sorts comments by creation date (descending)', function () {
+            var sorted = filterFactory(mock, 'Chronological');
+
+            expect(sorted[0].created).toBe("2013-04-19T04:04:55+0000");
+            expect(sorted[1].created).toBe("2013-04-18T04:04:55+0000");
+            expect(sorted[2].created).toBe("2013-04-17T23:53:19+0000");
+            expect(sorted[3].created).toBe("2013-04-17T20:25:53+0000");
+            expect(sorted[4].created).toBe("2013-02-12T16:22:45+0000");
+        });
+    });
+
+    describe('creating unknown filter version', function () {
+        var filterFactory;
+
+        beforeEach(inject(function ($filter) {
+            filterFactory = $filter('sortComments');
+        }));
+
+        it('throws an error', function () {
+            expect(function () {
+                filterFactory(mock, 'unknwonFilterVersion');
+            }).toThrow();
+        });
     });
 
 });
