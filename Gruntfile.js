0d4fe6e3e336ca480ace52d834f0873a6754e531
Yorick Terweijden <yorick.terweijden@sourcefabric.org>
2014-03-25 18:19:00 +0100

    Server task

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,13 +1,6 @@
 'use strict';
 
-// # Globbing
-// for performance reasons we're only matching one level down:
-// 'test/spec/{,*/}*.js'
-// use this if you want to recursively match all subfolders:
-// 'test/spec/**/*.js'
-
 module.exports = function (grunt) {
-  // require('load-grunt-tasks')(grunt);
   require('time-grunt')(grunt);
   require('load-grunt-config')(grunt, {
     data: {
@@ -15,19 +8,4 @@ module.exports = function (grunt) {
       dist: 'dist'
     }
   });
-
-  // grunt.registerTask('server', function (target) {
-  //   if (target === 'dist') {
-  //     return grunt.task.run(['build', 'connect:dist:keepalive']);
-  //   }
-
-  //   grunt.task.run([
-  //     'clean:server',
-  //     'concurrent:server',
-  //     'autoprefixer',
-  //     'connect:livereload',
-  //     'open',
-  //     'watch'
-  //   ]);
-  // });
 };
