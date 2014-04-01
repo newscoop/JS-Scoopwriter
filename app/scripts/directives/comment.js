795920c37b52310acd3b6dc99737afc2fd0d0228
Peter Lamut <peter.lamut@niteoweb.com>
2014-04-01 23:07:32 +0200

    AENV-149: simplify 'replyTo enabled' logic in comment template

--- app/scripts/directives/comment.js
+++ app/scripts/directives/comment.js
@@ -1,9 +1,13 @@
 'use strict';
+
 angular.module('authoringEnvironmentApp').directive('comment', function () {
     return {
         templateUrl: 'views/comment.html',
         restrict: 'E',
-        scope: { model: '=' },
+        scope: {
+            model: '=',
+            allowreplying: '='
+        },
         link: function postLink(scope, element, attrs) {
         }
     };
