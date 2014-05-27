70dd1e2c9544629e4dfc87f6e558d3657436fed3
Peter Lamut <peter.lamut@niteoweb.com>
2014-05-27 13:29:03 +0200

    Prevent user to select duplicate roles for the same author on the same article

--- app/scripts/filters/availableRoles.js
+++ app/scripts/filters/availableRoles.js
@@ -1,18 +1,63 @@
 'use strict';
 
 /**
-* AngularJS Filter for filtering out roles that particular author already has
-* assigned on the given article.
+* AngularJS Filter for filtering out roles that a particular author already has
+* assigned on the given article and thus these roles cannot be selected again
+* for the said author.
 *
 * @class availableRoles
 */
 angular.module('authoringEnvironmentApp').filter('availableRoles', [
     function () {
 
-        //TODO: docstrings and tests for this function
-        return function (input) {
-            // console.log('availableRoles filter applied');
-            return input;
+        /**
+        * From a given list of roles it filters out all those roles that are
+        * already assigned to the given author on a specific article and
+        * thus cannot be assigned again to the same author (on that article).
+        *
+        * @method availableRoles
+        * @param roleList {Array} list of all roles (unfiltered) that can be
+        *     assigned to article author
+        * @param author {Object} author for which to filter the role list
+        * @param authorList {Array} list of all current article authors
+        *   (with their roles) on a specific article
+        * @return {Array} list of filtered roles
+        */
+        return function (roleList, author, authorList) {
+            var filteredRoles = [];
+
+            if (!author) {
+                // no author selected (e.g. in live search widget), thus
+                // all roles are available
+                return roleList;
+            }
+
+            roleList.forEach(function (roleItem) {
+                var roleTaken = false;  // role already "taken" by the author?
+
+                authorList.forEach(function (authorItem) {
+                    // don't filter out the current role of the given
+                    // author's object instance...
+                    if (authorItem === author) {
+                        return;
+                    }
+
+                    // ...but do filter out a role if it is assigned to
+                    // *another* object instance representing the same author
+                    if (authorItem.id === author.id &&
+                        authorItem.articleRole.id === roleItem.id
+                    ) {
+                        roleTaken = true;
+                        return;
+                    }
+                });
+
+                if (!roleTaken) {
+                    filteredRoles.push(roleItem);
+                }
+            });
+
+            return filteredRoles;
         };
     }
 ]);
--- test/spec/filters/availableRoles.js
+++ test/spec/filters/availableRoles.js
@@ -8,13 +8,85 @@
 
 describe('Filter: availableRoles', function () {
 
-    var availableRoles;
+    var availableRoles,
+        roles;
 
     beforeEach(module('authoringEnvironmentApp'));
 
     beforeEach(inject(function ($filter) {
         availableRoles = $filter('availableRoles');
+
+        roles = [
+            {id: 1, name: 'Writer'},
+            {id: 2, name: 'Editor'},
+            {id: 4, name: 'Photographer'},
+            {id: 9, name: 'Programmer'},
+            {id: 13, name: 'Lector'},
+        ];
     }));
 
-    // TODO: tests here
+    it('does not filter anything if author is not given', function () {
+        var authorList,
+            filteredRoles;
+
+        authorList = [
+            {id:40, articleRole: {id: 4}},
+            {id:10, articleRole: {id: 1}},
+            {id:10, articleRole: {id: 2}},
+            {id:90, articleRole: {id: 9}},
+        ];
+
+        filteredRoles = availableRoles(roles, null, authorList);
+
+        expect(filteredRoles).toEqual(roles);
+    });
+
+    it('filters out all roles already assigned to author', function () {
+        var author,
+            authorList,
+            filteredRoles;
+
+        author = {id:10, articleRole: {id: 456}};
+
+        authorList = [
+            {id:40, articleRole: {id: 4}},
+            {id:10, articleRole: {id: 2}},
+            {id:10, articleRole: {id: 4}},
+            {id:10, articleRole: {id: 13}},
+            {id:90, articleRole: {id: 9}},
+        ];
+
+        filteredRoles = availableRoles(roles, author, authorList);
+
+        expect(filteredRoles).toEqual([
+            {id: 1, name: 'Writer'}, {id: 9, name: 'Programmer'}
+        ]);
+    });
+
+    it('does *not* filter out current role of the given author instance',
+        function () {
+            var author,
+                authorList,
+                filteredRoles;
+
+            authorList = [
+                {id:40, articleRole: {id: 4}},
+                {id:10, articleRole: {id: 2}},
+                {id:10, articleRole: {id: 4}},
+                {id:10, articleRole: {id: 13}},
+                {id:90, articleRole: {id: 9}},
+            ];
+
+            author = authorList[1];
+
+            filteredRoles = availableRoles(roles, author, authorList);
+
+            expect(filteredRoles).toEqual([
+                {id: 1, name: 'Writer'},
+                {id: 2, name: 'Editor'},
+                {id: 9, name: 'Programmer'}
+            ]);
+        }
+    );
+
 });
