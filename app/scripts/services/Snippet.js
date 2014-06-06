fb8d998a2bfa8e3fac6ce1bcddadbd505dc41c0a
Peter Lamut <peter.lamut@niteoweb.com>
2014-06-06 16:32:47 +0200

    Snippet service: remove dependency on $resource, add a few method stubs

--- app/scripts/services/Snippet.js
+++ app/scripts/services/Snippet.js
@@ -7,40 +7,60 @@
 */
 'use strict';
 angular.module('authoringEnvironmentApp').factory('Snippet', [
-    '$resource',
-    function ($resource) {
-        return {
-            all: $resource('/api/snippets/:template'),
-            single: $resource('/api/snippets/:template/:id', {}, {
-                get: {
-                    method: 'GET',
-                    params: {
-                        template: 'generic',
-                        id: ''
-                    },
-                    isArray: false
-                }
-            }),
-            article: $resource('/api/article/:articleId/:articleLanguage/snippets/:id', {}, {
-                all: {
-                    method: 'GET',
-                    params: {
-                        articleId: '',
-                        articleLanguage: ''
-                    },
-                    isArray: false
-                },
-                specific: {
-                    method: 'GET',
-                    params: {
-                        articleId: '',
-                        articleLanguage: '',
-                        id: ''
-                    },
-                    isArray: false
-                }
-            })
-        };    // // Service logic
+    function () {
+        var self = this,
+            Snippet = function () {};
+
+        // class methods
+
+        // TODO: comments and tests
+        Snippet.getAll = function (articleId, articleLang) {
+            // TODO: return empty array object, populate it when
+            // data gets there ... also this array should have $promise
+            // property
+
+            // GET '/api/snippets/:template'
+        };
+
+        // instance methods
+        Snippet.prototype.foo = function () {
+
+        };
+
+        return Snippet;
+
+        // return {
+        //     all: $resource('/api/snippets/:template'),
+        //     single: $resource('/api/snippets/:template/:id', {}, {
+        //         get: {
+        //             method: 'GET',
+        //             params: {
+        //                 template: 'generic',
+        //                 id: ''
+        //             },
+        //             isArray: false
+        //         }
+        //     }),
+        //     article: $resource('/api/article/:articleId/:articleLanguage/snippets/:id', {}, {
+        //         all: {
+        //             method: 'GET',
+        //             params: {
+        //                 articleId: '',
+        //                 articleLanguage: ''
+        //             },
+        //             isArray: false
+        //         },
+        //         specific: {
+        //             method: 'GET',
+        //             params: {
+        //                 articleId: '',
+        //                 articleLanguage: '',
+        //                 id: ''
+        //             },
+        //             isArray: false
+        //         }
+        //     })
+        // };    // // Service logic
               // // ...
               // var meaningOfLife = 42;
               // // Public API here
