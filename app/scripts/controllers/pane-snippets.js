ab7f364b7e1ec6e9565088f7750dee3319e73539
Peter Lamut <peter.lamut@niteoweb.com>
2014-06-09 15:43:35 +0200

    AENV-201: add Snippet.getAllByArticle() method, simplify controller

--- app/scripts/controllers/pane-snippets.js
+++ app/scripts/controllers/pane-snippets.js
@@ -1,70 +1,45 @@
 'use strict';
 angular.module('authoringEnvironmentApp').controller('PaneSnippetsCtrl', [
     '$scope',
-    function ($scope) {
-        function decorate(snippet) {
-            snippet.editing = false;
-            snippet.expanded = false;
-            snippet.edit = function () {
-                this.backup();
-                this.editing = true;
-                this.expanded = true;
-            };
-            snippet.backup = function () {
-                this.copy = {
-                    title: this.title,
-                    code: this.code
-                };
-            };
-            snippet.restore = function () {
-                this.title = this.copy.title;
-                this.code = this.copy.code;
-            };
-            snippet.cancel = function () {
-                this.editing = false;
-                this.restore();
-            };
-            return snippet;
-        }
-        $scope.snippets = [
-            {
-                id: 1,
-                title: 'Sound of the story',
-                code: '<iframe width="100%" height="166" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/128775977&amp;color=ff6600&amp;auto_play=false&amp;show_artwork=true"></iframe>'
-            },
-            {
-                id: 2,
-                title: 'Sound of the story 2',
-                code: '<iframe width="100%" height="166" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/128775977&amp;color=ff6600&amp;auto_play=false&amp;show_artwork=true"></iframe>'
-            },
-            {
-                id: 3,
-                title: 'Sound of the story 3',
-                code: '<iframe width="100%" height="166" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/128775977&amp;color=ff6600&amp;auto_play=false&amp;show_artwork=true"></iframe>'
-            }
-        ].map(decorate);
-        $scope.byId = function (id) {
-            return _.find($scope.snippets, function (s) {
-                return s.id === id;
-            });
-        };
-        $scope.create = {};
-        $scope.add = function () {
-            $scope.snippets.push(decorate({
-                title: $scope.create.title,
-                code: $scope.create.code
-            }));
-            $scope.create = {};
-            $scope.adding = false;
-        };
-        $scope.cancel = function () {
-            $scope.create = {};
-            $scope.adding = false;
-        };
-        $scope.remove = function (id) {
-            _.remove($scope.snippets, function (s) {
-                return s.id === id;
-            });
-        };
+    'article',
+    'Snippet',
+    function ($scope, article, Snippet) {
+
+        // $scope.byId = function (id) {
+        //     return _.find($scope.snippets, function (s) {
+        //         return s.id === id;
+        //     });
+        // };
+        // $scope.create = {};
+        // $scope.add = function () {
+        //     $scope.snippets.push(decorate({
+        //         title: $scope.create.title,
+        //         code: $scope.create.code
+        //     }));
+        //     $scope.create = {};
+        //     $scope.adding = false;
+        // };
+        // $scope.cancel = function () {
+        //     $scope.create = {};
+        //     $scope.adding = false;
+        // };
+        // $scope.remove = function (id) {
+        //     _.remove($scope.snippets, function (s) {
+        //         return s.id === id;
+        //     });
+        // };
+
+        // init: retrieve all article snippets from server
+        article.promise.then(function (articleData) {
+            $scope.snippets = Snippet.getAllByArticle(
+                articleData.number, articleData.language);
+
+            // TODO: add event listeners/handlers on success? or is this done
+            // in template, wiring the logic? probably better so that
+            // this code stays simple (avoiding callback in controller)
+
+            // ... or maybe add watch and decorate new array
+            // items as needed
+        });
     }
 ]);
--- app/scripts/services/Snippet.js
+++ app/scripts/services/Snippet.js
@@ -5,69 +5,104 @@
 *
 * @class Snippet
 */
-'use strict';
 angular.module('authoringEnvironmentApp').factory('Snippet', [
-    function () {
-        var self = this,
+    '$http',
+    '$q',
+    'configuration',
+    function ($http, $q, configuration) {
+        var API_ROOT = configuration.API.full,
+            self = this,
             Snippet = function () {};
 
-        // class methods
+        var snippetsResponseMock = [
+            {
+                id: 1,
+                title: 'Sound of the story',
+                code: '<iframe width="100%" height="166" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/128775977&amp;color=ff6600&amp;auto_play=false&amp;show_artwork=true"></iframe>'
+            },
+            {
+                id: 2,
+                title: 'Sound of the story 2',
+                code: '<iframe width="100%" height="166" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/128775977&amp;color=ff6600&amp;auto_play=false&amp;show_artwork=true"></iframe>'
+            },
+            {
+                id: 3,
+                title: 'Sound of the story 3',
+                code: '<iframe width="100%" height="166" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/128775977&amp;color=ff6600&amp;auto_play=false&amp;show_artwork=true"></iframe>'
+            }
+        ];
 
-        // TODO: comments and tests
-        Snippet.getAll = function (articleId, articleLang) {
-            // TODO: return empty array object, populate it when
-            // data gets there ... also this array should have $promise
-            // property
-
-            // GET '/api/snippets/:template'
+        /**
+        * Converts plain snippet data object to a new Snippet instance.
+        *
+        * @method createFromApiData
+        * @param data {Object} plan object containing snippet data
+        *   (as returned by API)
+        * @return {Object} Snippet resource object
+        */
+        self.createFromApiData = function (data) {
+            var snippet = Object.create(Snippet.prototype);
+            // TODO: copy properties as needed
+            ['id', 'title', 'code'].forEach(function (attribute) {
+                snippet[attribute] = data[attribute];
+            });
+            return snippet;
         };
 
-        // instance methods
-        Snippet.prototype.foo = function () {
+        /**
+        * Retrieves a list of all snippets attached to a specific article.
+        * Returned array has a special $promise property which is resolved or
+        * rejected depending on the server response.
+        *
+        * @method getAllByArticle
+        * @param number {Number} article ID
+        * @param language {String} article language code, e.g. 'de'
+        * @return {Object} "future" array of Snippet objects - initially
+        *   an empty array is returned, which is later populated with the
+        *   actual data (once the http promise has been successfully resolved)
+        */
+        Snippet.getAllByArticle = function (number, language) {
 
+            var deferredGet = $q.defer(),
+                snippets = [],
+                url;
+
+            url = [
+                API_ROOT, 'snippets', 'article', number, language
+            ].join('/');
+
+            snippets.$promise = deferredGet.promise;
+
+            $http.get(url, {
+                params: {
+                    items_per_page: 99999  // de facto "all"  TODO: needed?
+                }
+            }).success(function (response) {
+                JSON.parse(response).items.forEach(function (item) {
+                    item = self.createFromApiData(item);
+                    snippets.push(item);
+                });
+                // XXX: resolve with data that you put into results array?
+                deferredGet.resolve();
+            }).error(function (data, status, headers, config) {
+                // TODO: just for now when API misbehaves for empty
+                // result sets
+                // pretend that request succeeded
+                snippetsResponseMock.forEach(function (item) {
+                    item = self.createFromApiData(item);
+                    snippets.push(item);
+                });
+                deferredGet.reject();  // XXX: give reason?
+            });
+
+            return snippets;
+        };
+
+        // instance methods  XXX: just an example, later remove foo()
+        Snippet.prototype.foo = function () {
+            console.log('method foo');
         };
 
         return Snippet;
-
-        // return {
-        //     all: $resource('/api/snippets/:template'),
-        //     single: $resource('/api/snippets/:template/:id', {}, {
-        //         get: {
-        //             method: 'GET',
-        //             params: {
-        //                 template: 'generic',
-        //                 id: ''
-        //             },
-        //             isArray: false
-        //         }
-        //     }),
-        //     article: $resource('/api/article/:articleId/:articleLanguage/snippets/:id', {}, {
-        //         all: {
-        //             method: 'GET',
-        //             params: {
-        //                 articleId: '',
-        //                 articleLanguage: ''
-        //             },
-        //             isArray: false
-        //         },
-        //         specific: {
-        //             method: 'GET',
-        //             params: {
-        //                 articleId: '',
-        //                 articleLanguage: '',
-        //                 id: ''
-        //             },
-        //             isArray: false
-        //         }
-        //     })
-        // };    // // Service logic
-              // // ...
-              // var meaningOfLife = 42;
-              // // Public API here
-              // return {
-              //   someMethod: function () {
-              //     return meaningOfLife;
-              //   }
-              // };
     }
 ]);
