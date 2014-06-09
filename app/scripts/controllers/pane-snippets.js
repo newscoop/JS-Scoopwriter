84effa9648b6116e8a9dd53293e241f3beaa4e08
Peter Lamut <peter.lamut@niteoweb.com>
2014-06-09 16:29:50 +0200

    AENV-201: refactor (read: simplify) snippets template and pane controller

--- app/scripts/controllers/pane-snippets.js
+++ app/scripts/controllers/pane-snippets.js
@@ -3,43 +3,58 @@ angular.module('authoringEnvironmentApp').controller('PaneSnippetsCtrl', [
     '$scope',
     'article',
     'Snippet',
-    function ($scope, article, Snippet) {
+    'modalFactory',
+    function ($scope, article, Snippet, modalFactory) {
 
-        // $scope.byId = function (id) {
-        //     return _.find($scope.snippets, function (s) {
-        //         return s.id === id;
-        //     });
-        // };
-        // $scope.create = {};
-        // $scope.add = function () {
-        //     $scope.snippets.push(decorate({
-        //         title: $scope.create.title,
-        //         code: $scope.create.code
-        //     }));
-        //     $scope.create = {};
-        //     $scope.adding = false;
-        // };
-        // $scope.cancel = function () {
-        //     $scope.create = {};
-        //     $scope.adding = false;
-        // };
-        // $scope.remove = function (id) {
-        //     _.remove($scope.snippets, function (s) {
-        //         return s.id === id;
-        //     });
-        // };
+        // TODO: init UI flags like editing, expanded etc. for
+        // snippets objects? on load and on adding new snippet
+        // (set $watchCollection, just like in authors?)
+        // (idea: perhaps wrap all UI flags into snippet.UI object
+        // snippet.UI.editing, snippet.UI.expanded, snippet.UI.copy)
 
-        // init: retrieve all article snippets from server
+        $scope.showAddSnippet = false;  // TODO: test default value
+
+        // TODO: comments
+        $scope.toggleEdit = function (snippet, editMode) {
+            snippet.editing = editMode;
+            snippet.expanded = editMode;
+
+            // TODO: copy snippet data and restore if user cancels
+            // editing
+        };
+
+        // TODO: comments
+        $scope.updateSnippet = function (snippet) {
+            console.log('$scope.updateSnippet()', snippet);
+
+            // TODO: save changes (idea: only if there was a change,
+            // otherwise no need to send a request to server)
+        };
+
+        // TODO: comments
+        $scope.confirmRemoveSnippet = function (snippet) {
+            var modal,
+                title,
+                text;
+
+            // XXX: for now these texts stays in the controller, but should be
+            // moved to some general config section at some point, when we
+            // implement it in some refactoring sprint
+            title = 'Do you really want to remove this snippet?';
+            text = 'Should you change your mind, the snippet can ' +
+                'always be added again.';
+
+            modal = modalFactory.confirmLight(title, text);
+
+            modal.result.then(function (data) {
+                console.log('TODO: remove snippet', snippet);
+            });
+        };
+
+        // initialization: retrieve all article snippets from server
         article.promise.then(function (articleData) {
             $scope.snippets = Snippet.getAllByArticle(
                 articleData.number, articleData.language);
-
-            // TODO: add event listeners/handlers on success? or is this done
-            // in template, wiring the logic? probably better so that
-            // this code stays simple (avoiding callback in controller)
-
-            // ... or maybe add watch and decorate new array
-            // items as needed
         });
     }
 ]);
