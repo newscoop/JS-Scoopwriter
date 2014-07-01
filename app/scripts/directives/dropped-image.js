bd75fb45a2bad4bfcca0b2dd71a0ecf5121d6d4a
Peter Lamut <peter.lamut@niteoweb.com>
2014-07-01 14:49:22 +0200

    AENV-216: move image style-changing code to droppedImage directive

--- app/scripts/directives/dropped-image.js
+++ app/scripts/directives/dropped-image.js
@@ -8,8 +8,9 @@
 */
 
 angular.module('authoringEnvironmentApp').directive('droppedImage', [
-    '$popover',
-    function ($popover) {
+    'configuration',
+    '$log',
+    function (configuration, $log) {
         return {
             restrict: 'A',
             // XXX: replace: true?
@@ -26,24 +27,99 @@ angular.module('authoringEnvironmentApp').directive('droppedImage', [
             },
             link: function postLink(scope, element, attrs, ctrl) {
                 var imageId,
-                    $element = $(element);
+                    $element = $(element),
+                    $toolbar;
 
                 imageId = parseInt(element.attr('data-id'), 10);
                 ctrl.init(imageId);
+
+                // TODO: watch scope.image and when initialized,
+                // get $toolbar!
 
                 // close button's onClick handler
                 $element.find('button.close').click(function (e) {
                     e.stopPropagation();
                     $element.remove();
                     ctrl.imageRemoved(imageId);  // notify controller
+                    // TODO: also remove toolbar?
                 });
 
-                // $popover(element, {
-                //     placement: 'top',
-                //     html: true,
-                //     template: 'views/popover-image.html'
-                // });
-            }
+                $element.click(function (e) {
+                    e.stopPropagation();
+
+                    // XXX: what about duplicate images? same image in
+                    // article body twice?
+                    $toolbar = $toolbar || $('#img-toolbar-' + imageId);
+
+                    // XXX: only provisional
+                    $toolbar.css('top', 'auto');
+                    $toolbar.css('left', 'auto');
+
+                    $toolbar.show();
+                    // TODO: calculate abs position (top, left)?
+                });
+
+                // TODO: comments & tests
+                scope.align = function (position) {
+                    var cssFloat,
+                        cssMargin,
+                        img = scope.image;
+
+                    switch (position) {
+                    case 'left':
+                        cssFloat = 'left';
+                        if (img.size !== 'big') {
+                            cssMargin = '0 2% 0 0';
+                        }
+                        break;
+                    case 'right':
+                        cssFloat = 'right';
+                        if (img.size !== 'big') {
+                            cssMargin = '0 0 0 2%';
+                        }
+                        break;
+                    case 'center':
+                        cssFloat = 'none';
+                        cssMargin = '0 auto';
+                        break;
+                    default:
+                        $log.warn('unknown image alignment:', position);
+                        return;
+                    }
+
+                    $element.css('float', cssFloat);
+                    $element.css('margin', cssMargin);
+                };
+
+                // TODO: comments & tests
+                // sizes: small, medium, big, original
+                scope.setSize = function (size) {
+                    // var img = scope.image;
+                    // img.size = size;  // TODO: later
+                    $element.width(configuration.image.width[size]);
+
+                    if (size === 'big') {
+                        $element.css('margin', '0');
+                    } else if (size === 'original') {
+                        $element.css('width', 'auto');
+                        // i.style.image.width = 'auto';
+                    } else {
+                        // TODO: what is meant to be here??
+                        // i.style.image = {};
+                    }
+                };
+
+                // TODO: comments & tests
+                scope.changePixelSize = function (width) {
+                    var newWidth;
+
+                    if (angular.isNumber(scope.pixels)) {
+                        newWidth = scope.pixels + 'px';
+                        // TODO: set this
+                        // i.style.image.width = p;
+                    }
+                };
+            }  // end postLink function
         };
     }
 ]);
--- test/spec/controllers/dropped-image.js
+++ test/spec/controllers/dropped-image.js
@@ -64,79 +64,79 @@ describe('Controller: DroppedImageCtrl', function () {
     it('proxies images', function () {
         expect(scope.images).toBeDefined();
     });
-    describe('image got', function() {
-        beforeEach(function() {
-            scope.get(3);
-            image.style = {
-                container: {},
-                image: {}
-            };
-        });
-        describe('image selected', function() {
-            beforeEach(function() {
-                scope.select(3);
-            });
-            it('sets the image as selected in the service', function() {
-                expect(images.selected).toBe(3);
-            });
-            /* the pitfall about many of the following settings is
-             * having them correcly set after several changes. for
-             * example changing the width to full screen may affect
-             * the margin, the positioning, etcetera. add more
-             * specific tests when you are in doubt. this is also why
-             * the whole style is checked in each test */
-            it('sets a big size', function() {
-                scope.size('big');
-                expect(images.included[3].style)
-                    .toEqual({ container : { width : '100%', margin : '0' }, image : {  } });
-            });
-            it('aligns a big image to the right', function() {
-                scope.size('big');
-                scope.align('right');
-                expect(images.included[3].style)
-                    .toEqual({ container : { width : '100%', margin : undefined, float : 'right' }, image : {  } });
-            });
-            it('sets original size', function() {
-                scope.size('original');
-                expect(images.included[3].style)
-                    .toEqual({ container : { width : 'auto' }, image : { width : 'auto' } });
-            });
-            it('sets another size', function() {
-                scope.size('whatever');
-                expect(images.included[3].style)
-                    .toEqual({ container : { width : undefined }, image : {  } });
-            });
-            it('sets a size in pixels', function() {
-                scope.pixels = 200;
-                scope.pixelsChanged();
-                expect(images.included[3].style)
-                    .toEqual({ container : {  }, image : { width : '200px' } });
-            });
-            it('aligns left', function() {
-                scope.align('left');
-                expect(images.included[3].style)
-                    .toEqual({ container : { float : 'left', margin : '0 2% 0 0' }, image: {}});
-            });
-            it('aligns right', function() {
-                scope.align('right');
-                expect(images.included[3].style)
-                    .toEqual({ container : { float : 'right', margin : '0 0 0 2%' }, image: {}});
-            });
-            it('aligns center', function() {
-                scope.align('center');
-                expect(images.included[3].style)
-                    .toEqual({ container : { float : 'none', margin : '0 auto' }, image : {  } });
-            });
-            describe('somebody changes the selected image', function() {
-                beforeEach(function() {
-                    images.selected = 4;
-                });
-                it('cannot change the size of a wrong image', function() {
-                    expect(function() {
-                        scope.size('big');
-                    }).toThrow();
-                });
-            });
-        });
-    });
+    // describe('image got', function() {
+    //     beforeEach(function() {
+    //         scope.get(3);
+    //         image.style = {
+    //             container: {},
+    //             image: {}
+    //         };
+    //     });
+    //     describe('image selected', function() {
+    //         beforeEach(function() {
+    //             scope.select(3);
+    //         });
+    //         it('sets the image as selected in the service', function() {
+    //             expect(images.selected).toBe(3);
+    //         });
+    //         /* the pitfall about many of the following settings is
+    //          * having them correcly set after several changes. for
+    //          * example changing the width to full screen may affect
+    //          * the margin, the positioning, etcetera. add more
+    //          * specific tests when you are in doubt. this is also why
+    //          * the whole style is checked in each test */
+    //         it('sets a big size', function() {
+    //             scope.size('big');
+    //             expect(images.included[3].style)
+    //                 .toEqual({ container : { width : '100%', margin : '0' }, image : {  } });
+    //         });
+    //         it('aligns a big image to the right', function() {
+    //             scope.size('big');
+    //             scope.align('right');
+    //             expect(images.included[3].style)
+    //                 .toEqual({ container : { width : '100%', margin : undefined, float : 'right' }, image : {  } });
+    //         });
+    //         it('sets original size', function() {
+    //             scope.size('original');
+    //             expect(images.included[3].style)
+    //                 .toEqual({ container : { width : 'auto' }, image : { width : 'auto' } });
+    //         });
+    //         it('sets another size', function() {
+    //             scope.size('whatever');
+    //             expect(images.included[3].style)
+    //                 .toEqual({ container : { width : undefined }, image : {  } });
+    //         });
+    //         it('sets a size in pixels', function() {
+    //             scope.pixels = 200;
+    //             scope.pixelsChanged();
+    //             expect(images.included[3].style)
+    //                 .toEqual({ container : {  }, image : { width : '200px' } });
+    //         });
+    //         it('aligns left', function() {
+    //             scope.align('left');
+    //             expect(images.included[3].style)
+    //                 .toEqual({ container : { float : 'left', margin : '0 2% 0 0' }, image: {}});
+    //         });
+    //         it('aligns right', function() {
+    //             scope.align('right');
+    //             expect(images.included[3].style)
+    //                 .toEqual({ container : { float : 'right', margin : '0 0 0 2%' }, image: {}});
+    //         });
+    //         it('aligns center', function() {
+    //             scope.align('center');
+    //             expect(images.included[3].style)
+    //                 .toEqual({ container : { float : 'none', margin : '0 auto' }, image : {  } });
+    //         });
+    //         describe('somebody changes the selected image', function() {
+    //             beforeEach(function() {
+    //                 images.selected = 4;
+    //             });
+    //             it('cannot change the size of a wrong image', function() {
+    //                 expect(function() {
+    //                     scope.size('big');
+    //                 }).toThrow();
+    //             });
+    //         });
+    //     });
+    // });
 });
