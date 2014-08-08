20474fd4de6e26210de656136bf823c0c3123d28
Peter Lamut <peter.lamut@niteoweb.com>
2014-08-08 17:43:29 +0200

    AENV-244: fix highlighting of editor's formatting buttons

--- app/scripts/directives/sf-aloha-format-generic.js
+++ app/scripts/directives/sf-aloha-format-generic.js
@@ -2,8 +2,16 @@
 angular.module('authoringEnvironmentApp').directive('sfAlohaFormatGeneric', [
     'AlohaFormattingFactory',
     function (AlohaFormattingFactory) {
+        var template = [
+            '<button class="btn btn-default btn-sm action strong" ',
+            '    rel="tooltip" title="{{buttonName}}">',
+            '    <i class="editoricon-{{alohaElement|lowercase}} ',
+            '              fa fa-{{alohaElement|lowercase}}"></i>',
+            '</button>'
+        ].join('');
+
         return {
-            template: '<button class="btn btn-default btn-sm action strong" rel="tooltip"' + 'title="{{buttonName}}"><i class="fa fa-{{alohaElement|lowercase}}"></i></button>',
+            template: template,
             restrict: 'A',
             replace: true,
             scope: {
@@ -12,7 +20,11 @@ angular.module('authoringEnvironmentApp').directive('sfAlohaFormatGeneric', [
             },
             link: function postLink(scope, element, attrs) {
                 AlohaFormattingFactory.add(scope.alohaElement);
-                element.attr('disabled', Aloha.queryCommandSupported(scope.alohaElement) && Aloha.queryCommandEnabled(scope.alohaElement));
+                element.attr(
+                    'disabled',
+                    Aloha.queryCommandSupported(scope.alohaElement) &&
+                    Aloha.queryCommandEnabled(scope.alohaElement)
+                );
                 element.bind('click', function () {
                     Aloha.execCommand(scope.alohaElement, false, '');
                     element.toggleClass('active', AlohaFormattingFactory.query(scope.alohaElement));
