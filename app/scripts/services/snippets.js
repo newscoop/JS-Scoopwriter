a6797348bbd867a6d1b2a00727a342b784c65d29
Peter Lamut <peter.lamut@niteoweb.com>
2014-08-13 17:12:33 +0200

    AENV-242: remove redundant stuff from snippets service

--- app/scripts/services/snippets.js
+++ app/scripts/services/snippets.js
@@ -1,13 +1,16 @@
 'use strict';
 
-// TODO: comment ... service for keeping  track of article snippets
-// attached to the article
+/**
+* AngularJS service for keeping track of which snippets are attached to the
+* current article being edited and which of those are used in article content.
+*
+* @class snippets
+*/
 angular.module('authoringEnvironmentApp').service('snippets', [
     '$log',
     'article',
     'Snippet',
     function ($log, article, Snippet) {
-        /* more info about the page tracker in its tests */
         var self = this;
 
         article.promise.then(function (article) {
@@ -63,9 +66,9 @@ angular.module('authoringEnvironmentApp').service('snippets', [
         };
 
         /**
-        * Attaches a single snippet to the article. If the snippet image is
-        * already attached, it does not do anything. On a successful server
-        * response it also updates the list of attached snippets.
+        * Attaches a single snippet to the article. If the snippet is already
+        * attached, it does not do anything. On successful server response
+        * it also updates the list of attached snippets.
         *
         * @method addToArticle
         * @param snippet {Object} Snippet instance to attach
@@ -90,14 +93,19 @@ angular.module('authoringEnvironmentApp').service('snippets', [
             return promise;
         };
 
+
         /**
-        * Detaches a single image from the article (using HTTP UNLINK). If the
-        * image is not attached to the article, it does not do anything.
+        * Detaches a single image from the article. If the snippet is not
+        * attached to the article, it does not do anything.
         *
-        * @method detach
-        * @param id {Number} ID of an image to detach
+        * @method removeFromArticle
+        * @param snippet {Object} Snippet instance to attach
+        * @param article {Object} article to which the snippet should
+        *   be attached.
+        *   @param article.number {Number} ID of the article
+        *   @param article.language {String} article language code (e.g. 'de')
         */
-        this.detach = function (id) {
+        self.removeFromArticle = function (snippet, article) {
             var match = this.matchMaker(id);
             console.log('detaching..');
             if (_.find(this.attached, match)) {
@@ -121,63 +129,5 @@ angular.module('authoringEnvironmentApp').service('snippets', [
                 $log.debug('image already detached, ignoring attach request');
             }
         };
-
-
-
-        /**
-        * Searches for an image with the given ID in the list of images
-        * attached to the article. If found, it returns the image.
-        *
-        * @method findAttached
-        * @param id {Number} ID of the image
-        * @return {Object|undefined} image object (undefined if image is not
-        *     found)
-        */
-        this.findAttached = function (id) {
-            return _.find(this.attached, this.matchMaker(id));
-        };
-
-        /**
-        * Searches for an image with the given ID in the list of images
-        * currently in the basket. If found, it returns the image.
-        *
-        * @method findCollected
-        * @param id {Number} ID of the image
-        * @return {Object|undefined} image object (undefined if image is not
-        *     found)
-        */
-        this.findCollected = function (id) {
-            return _.find(this.collected, this.matchMaker(id));
-        };
-
-        /**
-        * Retrieves an image from the list of images attached to the article.
-        * If the image is not found, it an error is raised.
-        *
-        * @method byId
-        * @param id {Number} ID of the image
-        * @return {Object} image object
-        */
-        this.byId = function (id) {
-            var i = this.findAttached(id);
-            if (i) {
-                return i;
-            } else {
-                throw new Error('asking details about an image which is not attached to the article is not supported');
-            }
-        };
-
-        /**
-        * Checks if image is currently attached to the article or not.
-        *
-        * @method isAttached
-        * @param id {Number} ID of the image
-        * @return {String} true if image is attached to the article,
-        *     false otherwise
-        */
-        this.isAttached = function (id) {
-            return typeof this.findAttached(id) !== 'undefined';
-        };
-
     }
 ]);
