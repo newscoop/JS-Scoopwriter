b2af1fa976b729a5894310e9bbd89b508d553ab1
Peter Lamut <peter.lamut@niteoweb.com>
2014-08-22 15:34:13 +0200

    100% test coverage for the mode service.

--- app/scripts/services/mode.js
+++ app/scripts/services/mode.js
@@ -1,37 +1,41 @@
 'use strict';
+
 /**
 * AngularJS Service for switching between article editing modes.
 *
 * @class mode
 */
 angular.module('authoringEnvironmentApp').service('mode', function mode() {
-    // AngularJS will instantiate a singleton by calling "new" on this function
+
     /**
-        * Current mode of editing ('normal' or 'zen')
-        * @property current
-        * @type String
-        * @default 'normal'
-        */
+    * Current mode of editing ('normal' or 'zen')
+    * @property current
+    * @type String
+    * @default 'normal'
+    */
     this.current = 'normal';
+
     /**
-        * A flag indicating whether we are in zen editing mode or not.
-        * @property zen
-        * @type Boolean
-        * @default false
-        */
+    * A flag indicating whether we are in zen editing mode or not.
+    * @property zen
+    * @type Boolean
+    * @default false
+    */
     this.zen = false;
+
     /**
-        * Switch to zen editing mode.
-        * @method goZen
-        */
+    * Switch to zen editing mode.
+    * @method goZen
+    */
     this.goZen = function () {
         this.current = 'zen';
         this.zen = true;
     };
+
     /**
-        * Switch to normal editing mode.
-        * @method goNormal
-        */
+    * Switch to normal editing mode.
+    * @method goNormal
+    */
     this.goNormal = function () {
         this.current = 'normal';
         this.zen = false;
