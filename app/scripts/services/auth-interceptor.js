5597b6e92d5f3b76dcc2754861dc35482733da8b
Peter Lamut <peter.lamut@niteoweb.com>
2014-08-25 12:00:04 +0200

    100% test coverage for authInterceptor service

--- app/scripts/services/auth-interceptor.js
+++ app/scripts/services/auth-interceptor.js
@@ -1,12 +1,22 @@
 'use strict';
+
+/**
+* AngularJS Service for intercepting API requests and adding authorization
+* info to them.
+*
+* @class authInterceptor
+*/
 angular.module('authoringEnvironmentApp').factory('authInterceptor', [
     '$q',
     '$window',
     function ($q, $window) {
         return {
             request: function (config) {
-                var endpoint = Routing.generate('newscoop_gimme_articles_getarticles');
+                var endpoint = Routing.generate(
+                    'newscoop_gimme_articles_getarticles', {}, false);
+
                 endpoint = endpoint.substring(1, endpoint.lastIndexOf('/'));
+
                 if (config.url.indexOf(endpoint + '/') === -1) {
                     // do not modify the headers of anything else but the API
                     return config;
@@ -15,7 +25,8 @@ angular.module('authoringEnvironmentApp').factory('authInterceptor', [
                 config.headers = config.headers || {};
 
                 if ($window.sessionStorage.token) {
-                    config.headers.Authorization = 'Bearer ' + $window.sessionStorage.token;
+                    config.headers.Authorization = 'Bearer ' +
+                        $window.sessionStorage.token;
                 }
 
                 return config;
--- test/spec/services/auth-interceptor.js
+++ test/spec/services/auth-interceptor.js
@@ -1,18 +1,73 @@
 'use strict';
 
+/**
+* Module with tests for the authInterceptor service.
+*
+* @module authInterceptor service tests
+*/
+
 describe('Service: AuthInterceptor', function () {
 
-  // load the service's module
-  beforeEach(module('authoringEnvironmentApp'));
+    var authInterceptor,
+        $window;
 
-  // instantiate service
-  var authInterceptor;
-  beforeEach(inject(function (_authInterceptor_) {
-    authInterceptor = _authInterceptor_;
-  }));
+    // load the service's module
+    beforeEach(module('authoringEnvironmentApp'));
 
-  it('should do something', function () {
-    expect(!!authInterceptor).toBe(true);
-  });
+    beforeEach(inject(function (_authInterceptor_, _$window_) {
+        authInterceptor = _authInterceptor_;
+        $window = _$window_;
+    }));
+
+    describe('request interceptor', function () {
+        it('implements request() method', function () {
+            expect(typeof authInterceptor.request).toBe('function');
+        });
+
+        it('does not modify non-API requests', function () {
+            var config,
+                returned;
+            config = {
+                url: 'http://foo.com/bar',
+                headers: {}
+            };
+            returned = authInterceptor.request(config);
+            expect(returned).toEqual(config);
+        });
+
+        it('does not modify API requests if session does not contain ' +
+           'authorization token',
+            function () {
+                var config,
+                    returned;
+
+                config = {
+                    url: 'http://backend.com/content-api/articles/8/en'
+                };
+                $window.sessionStorage.token = undefined;
+
+                returned = authInterceptor.request(config);
+                expect(returned).toEqual(config);
+            }
+        );
+
+        it('sets authorization header for API requests if session ' +
+           'contains authorization token',
+            function () {
+                var config,
+                    returned;
+
+                config = {
+                    url: 'http://backend.com/content-api/articles/8/en',
+                    headers: {}
+                };
+                $window.sessionStorage.token = 'abcd1234';
+
+                returned = authInterceptor.request(config);
+                expect(returned.headers.Authorization).toEqual(
+                    'Bearer abcd1234');
+            }
+        );
+    });
 
 });
