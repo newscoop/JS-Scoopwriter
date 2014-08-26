5bf582a7c8524f3af9d4bfea2454addda1b1ecaa
Peter Lamut <peter.lamut@niteoweb.com>
2014-08-26 17:13:06 +0200

    100% test coverage for AlohaFormattingFactory

--- app/scripts/services/AlohaFormattingFactory.js
+++ app/scripts/services/AlohaFormattingFactory.js
@@ -1,4 +1,5 @@
 'use strict';
+
 /**
 * AngularJS Factory for creating formatting objects for the Aloha editor.
 *
@@ -7,42 +8,79 @@
 angular.module('authoringEnvironmentApp').factory('AlohaFormattingFactory', [
     '$rootScope',
     function ($rootScope) {
-        // Service logic
-        // ...
+
         // Whenever there is a change in editor's content selection, update
-        // display statuses of editor's formatting buttons depending on whether or
-        // not the corresponding formatting is in effect for the selected content.
+        // display statuses of editor's formatting buttons depending on
+        // whether or not the corresponding formatting is in effect for the
+        // currently selected content.
         $rootScope.$on('texteditor-selection-changed', function () {
             angular.forEach(formatters, function (value, key) {
                 var selected = Aloha.queryCommandState(value);
-                jQuery('.editoricon-' + value.toLowerCase()).parent().toggleClass('active', selected);
+                jQuery(
+                    '.editoricon-' + value.toLowerCase()
+                ).parent().toggleClass('active', selected);
             });
         });
-        var formatters = [];
+
         // list of names of available content formatters
+        var formatters = [];
+
         // list of names of forbidden content formatters
         var forbidden = [
                 'Insertorderedlist',
                 'Insertunorderedlist'
             ];
+
         // Public API here
         return {
+            /**
+            * Returns a list of names of available content formatters.
+            *
+            * @method get
+            * @return {Array} list available formatters' names
+            */
             get: function () {
                 return formatters;
             },
-            add: function (Formatter) {
-                if (forbidden.indexOf(Formatter) === -1) {
-                    formatters.push(Formatter);
+
+            /**
+            * Adds formatter to the list of available content formatters. If
+            * formater is forbidden, it is not added.
+            *
+            * @method add
+            * @param formatter {String} name of the formatter
+            */
+            add: function (formatter) {
+                if (forbidden.indexOf(formatter) === -1) {
+                    formatters.push(formatter);
                 }
             },
-            remove: function (Formatter) {
-                // XXX: this removes the last element of the array if Formatter is
-                // not found in formatters - bug or a feature? Probably a bug?
-                formatters.splice(formatters.indexOf(Formatter), 1);
+
+            /**
+            * Removes formatter from the list of available content formatters.
+            *
+            * @method remove
+            * @param formatter {String} name of the formatter
+            */
+            remove: function (formatter) {
+                var idx = formatters.indexOf(formatter);
+                if (idx > -1) {
+                    formatters.splice(idx, 1);
+                }
             },
-            query: function (Formatter) {
-                if (forbidden.indexOf(Formatter) === -1) {
-                    return Aloha.queryCommandState(Formatter);
+
+            /**
+            * Checks if particular formatter is currently active. For forbidden
+            * formatters it always returns false.
+            *
+            * @method query
+            * @param formatter {String} name of the formatter
+            * @return {Boolean} true if formatter is currently active,
+            *   false otherwise
+            */
+            query: function (formatter) {
+                if (forbidden.indexOf(formatter) === -1) {
+                    return Aloha.queryCommandState(formatter);
                 } else {
                     return false;
                 }
