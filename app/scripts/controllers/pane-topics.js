fd68f7960cec046a8cfc52102c5e475eda7ca264
Peter Lamut <peter.lamut@niteoweb.com>
2014-09-09 16:45:36 +0200

    AENV-255: create initial topics pane content

--- app/scripts/controllers/pane-topics.js
+++ app/scripts/controllers/pane-topics.js
@@ -1,5 +1,10 @@
 'use strict';
 
+/**
+* AngularJS controller for the Topics pane.
+*
+* @class PaneTopicsCtrl
+*/
 angular.module('authoringEnvironmentApp').controller('PaneTopicsCtrl',
     function ($scope) {
         $scope.topics = [
