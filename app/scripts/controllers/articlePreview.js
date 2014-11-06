50745dee7594c68fdceac165a83ccb5ac2417488
Peter Lamut <peter.lamut@niteoweb.com>
2014-11-06 17:52:13 +0100

    AENV-279: open a modal on clicking the article preview button

--- app/scripts/controllers/articlePreview.js
+++ app/scripts/controllers/articlePreview.js
@@ -1,13 +1,59 @@
-'use strict';
+(function () {
+    'use strict';
 
-/**
-* AngularJS controller for displaying a modal containing the article preview.
-*
-* @class ArticlePreviewCtrl
-*/
-angular.module('authoringEnvironmentApp').controller('ArticlePreviewCtrl', [
-    'article',
-    function (article) {
-        console.debug('this is ArticlePreviewCtrl');
+    /**
+    * Constructor function for the article preview modal controller.
+    *
+    * @class ModalCtrl
+    */
+    function ModalCtrl($scope, $modalInstance, articleInfo) {
+        var self = this;
+
+        self.articleInfo = articleInfo;
+
+        /**
+        * Closes the modal.
+        * @method close
+        */
+        self.close = function () {
+            $modalInstance.close();
+        };
     }
-]);
+
+    ModalCtrl.$inject = ['$scope', '$modalInstance', 'articleInfo'];
+
+
+    /**
+    * AngularJS controller for displaying a modal containing article preview.
+    *
+    * @class ArticlePreviewCtrl
+    */
+    angular.module('authoringEnvironmentApp')
+    .controller('ArticlePreviewCtrl', [
+        '$modal',
+        'article',
+        function ($modal, article) {
+            var self = this;
+
+            // TODO: need to wait until the article is loaded...
+            // article.promise.then
+            self.openPreview = function () {
+                $modal.open({
+                    templateUrl: 'views/modal-article-preview.html',
+                    controller: ModalCtrl,
+                    controllerAs: 'modalPreviewCtrl',
+                    resolve: {
+                        articleInfo: function () {
+                            return {
+                                articleId: article.articleId,
+                                language: article.language,
+                                foo: 'bar'
+                            };
+                        },
+                    }
+                });
+            };
+        }
+    ]);
+
+}());
