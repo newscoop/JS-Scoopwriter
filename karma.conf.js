bfebd1c36ab72c2b7e7a0ead8cdcf82899a6a221
Peter Lamut <peter.lamut@niteoweb.com>
2014-12-04 13:58:04 +0100

    Remove unneeded files related to old auth. mechanism

--- karma.conf.js
+++ karma.conf.js
@@ -31,7 +31,6 @@ module.exports = function(config) {
       'app/bower_components/ng-file-upload/angular-file-upload.js',
       'app/bower_components/select2/select2.js',
       'app/bower_components/angular-ui-select2/src/select2.js',
-      'app/bower_components/angular-oauth/src/js/angularOauth.js',
       'app/bower_components/gc-angular-toaster/toaster.js',
       'app/scripts/*.js',
       'app/scripts/services/*.js',
