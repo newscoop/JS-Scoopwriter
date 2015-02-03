684e705c5f00d46e4e2bdeb27e3199fc328048e4
Mark Lewis <thnkloud9@gmail.com>
2015-02-03 15:57:50 +0100

    add editor change events for dropped-image functions

--- app/scripts/directives/dropped-image.js
+++ app/scripts/directives/dropped-image.js
@@ -85,6 +85,25 @@ angular.module('authoringEnvironmentApp').directive('droppedImage', [
                     toolbarNode().css({left: left, top: top});
                 }
 
+                /**
+                * Emits an aloha event which triggers the editor save button
+                *
+                * @function triggerChangeEvent
+                */
+                function triggerChangeEvent() {
+                    // emit text editor change event
+                    var alohaEditable = Aloha.getEditableById(
+                        $parent.parent('.aloha-editable-active').attr('id')
+                    );
+                    if (alohaEditable) {
+                        Aloha.trigger('aloha-smart-content-changed', {
+                            'editable': alohaEditable,
+                            'triggerType': 'paste',
+                            'snapshotContent': alohaEditable.getContents()
+                        });
+                    }
+                };
+
 
                 // close button's onClick handler
                 $element.find('button.close').click(function (e) {
@@ -156,6 +175,7 @@ angular.module('authoringEnvironmentApp').directive('droppedImage', [
 
                     $parent.attr('data-align', position);
 
+                    triggerChangeEvent();
                     positionToolbar();
                 };
 
@@ -206,6 +226,7 @@ angular.module('authoringEnvironmentApp').directive('droppedImage', [
                         scope.widthPx -= 2;
                     }
 
+                    triggerChangeEvent();
                     positionToolbar();
                 };
 
@@ -233,11 +254,12 @@ angular.module('authoringEnvironmentApp').directive('droppedImage', [
 
                         scope.widthPx = width;
                         scope.activeSize = 'custom';
-
+                        
+                        triggerChangeEvent();
                         positionToolbar();
                     }
                 };
-
+                
                 // set default values if needed and set image properties
                 // XXX: for some reason the directive is sometimes fired twice
                 // and values in scope (except for articleImageId) get lost.
