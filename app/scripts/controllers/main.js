3a99bbc9ed51c3db54665f0eb593184318504eb7
Rafał Muszyński <rmuszynski1@gmail.com>
2015-05-20 16:37:58 +0200

    obtain token via API, first

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -11,13 +11,19 @@ controller('MainCtrl', [
             if (!userAuth.isAuthenticated()) {
                 $scope.auth = false;
 
-                var promise = userAuth.newTokenByLoginModal();
+                var promise = userAuth.obtainToken();
 
                 promise.then(function(token) {
                     $scope.auth = true;
                 })
                 .catch(function () {
-                    // XXX: show toast message?
+                    var promise = userAuth.newTokenByLoginModal();
+                    promise.then(function(token) {
+                        $scope.auth = true;
+                    })
+                    .catch(function () {
+                        // XXX: show toast message?
+                    });
                 });
             } else {
                 $scope.auth = true;
