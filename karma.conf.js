ec9f22b7efc27da8d4c1526b56e83249d1c8e0b4
Rafał Muszyński <rmuszynski1@gmail.com>
2015-07-15 17:18:21 +0200

    increase browser no activity timeout

--- karma.conf.js
+++ karma.conf.js
@@ -9,7 +9,7 @@ module.exports = function(config) {
     frameworks: ['jasmine'],
     browserDisconnectTimeout: 10000,
     browserDisconnectTolerance: 2,
-    browserNoActivityTimeout: 120000,
+    browserNoActivityTimeout: 180000,
 
     // list of files / patterns to load in the browser
     files: [
