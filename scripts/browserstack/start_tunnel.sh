15305475a151baf4966bf673bdde693019352f33
Rafał Muszyński <rmuszynski1@gmail.com>
2015-07-21 14:47:22 +0200

    test

--- scripts/browserstack/start_tunnel.sh
+++ scripts/browserstack/start_tunnel.sh
@@ -3,7 +3,6 @@
 set -e
 
 if [ "$BROWSER_PROVIDER" == "browserstack" ]; then
-	cd .. && cd .. && cd .. && travis_stop_sauce_connect
 	wget http://www.browserstack.com/browserstack-local/BrowserStackLocal-linux-x64.zip
 	unzip BrowserStackLocal-linux-x64.zip
 	./BrowserStackLocal $BROWSER_STACK_ACCESS_KEY localhost,8001,0 &
