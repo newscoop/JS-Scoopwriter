70e2bd192a68d93c4ccb3d41273ac36f66be87cc
Rafał Muszyński <rmuszynski1@gmail.com>
2015-07-21 11:01:48 +0200

    browserstack local

--- scripts/browserstack/start_tunnel.sh
+++ scripts/browserstack/start_tunnel.sh
@@ -6,6 +6,5 @@ if [ "$BROWSER_PROVIDER" == "browserstack" ]; then
 	#node ./scripts/browserstack/start_tunnel.js &
 	wget http://www.browserstack.com/browserstack-local/BrowserStackLocal-linux-x64.zip
 	unzip BrowserStackLocal-linux-x64.zip
-	./BrowserStackLocal $BROWSER_STACK_ACCESS_KEY localhost,8001,0 > /dev/null &
-	sleep 10
+	./BrowserStackLocal $BROWSER_STACK_ACCESS_KEY localhost,8001,0 &
 fi
